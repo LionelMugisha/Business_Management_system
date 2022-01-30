ac8d8743f31d131d4a3ae8482f54e1b11d0584df
LionelMugisha <lionelmugisha2@gmail.com>
2022-01-30 18:37:19 +0200

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,42 @@
+<x-layout>
+    <div class="flex items-center min-h-screen bg-gray-50">
+        <div class="flex-1 h-full max-w-4xl mx-auto bg-white rounded-lg shadow-xl">
+            <div class="flex flex-col md:flex-row">
+                <div class="h-32 md:h-auto md:w-1/2">
+                    <img class="object-cover w-full h-full" src="https://source.unsplash.com/user/erondu/1600x900" alt="img" />
+                </div>
+                <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
+                    <div class="w-full">
+                        <h1 class="mb-4 text-2xl font-bold text-center text-gray-700">
+                            Login to Your Account
+                        </h1>
+                        <form method="POST" action="/">
+                            @csrf
+                            <div>
+                                <label class="block text-sm">Email</label>
+                                <input type="email" id="email" name="email" value="{{ old('email') }}" class="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600" placeholder="Enter your E-mail" required />
+                                @error('email')
+                                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                                @enderror
+                            </div>
+                            <div>
+                                <label class="block mt-4 text-sm">Password</label>
+                                <input type="password" id="password" name="password" class="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600" placeholder=" **************** " required />
+                                @error('password')
+                                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                                @enderror
+                            </div>
+                            <button type="submit" class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue">
+                                Log in
+                            </button>
+                        </form>
+
+
+                        <hr class="my-8" />
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
